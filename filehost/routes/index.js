e6b61f57eb78aabfe0c2ebfc15d227905f74febd
AndreyKrysyuk <andrey130198@gmail.com>
2016-11-23 10:10:22 +0200

    authorization

--- filehost/routes/index.js
+++ filehost/routes/index.js
@@ -1,8 +1,70 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
+var mpromise = require('mpromise');
 var crypto = require('crypto');
 var UserModel = require('../user_model').UserModel;
+
+const bodyParser = require('body-parser');
+const busboyBodyParser = require('busboy-body-parser');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
+let sessionSecret = "jahdgalsdg^&(*&^%  _Asds)";
+
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
+router.use(busboyBodyParser({ limit: '5mb' }));
+router.use(cookieParser());
+router.use(session({
+	secret: sessionSecret,
+	resave: false,
+	saveUninitialized: true
+}));
+router.use(passport.initialize());
+router.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+    done(null, user._id);
+});
+
+passport.deserializeUser(function(id, done) {
+	console.log("deserializeUser id: " + id);
+	UserModel.findOne({ _id: id}).exec((err, user) => {
+    if(err){
+      done(err, null);
+    } else {
+      if(user) {
+				done(null, user);
+			} else {
+				done("No user", null);
+			}
+    }
+  });
+});
+
+passport.use(new LocalStrategy((username, password, done) => {
+	  console.log("Local: " + username + " : " + password);
+	  UserModel.findOne({
+			  username: username,
+			  password: hash(password)
+		  }).exec((err, user) => {
+        if(!err){
+          console.log(user);
+  				if (user) {
+  					done(null, user);
+  				} else {
+  					done(null, false);
+  				}
+        } else {
+          console.log(err);
+  				done(err, null);
+        }
+      });
+}));
+
 
 var salt = '13daghrnek#&$^@:"FSDK"!.vbn`139573';
 function hash(pass){
@@ -10,7 +72,8 @@ function hash(pass){
 }
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('main');
+	if(req.user) res.render('main_s');
+	else res.render('main');
 });
 
 router.get('/register', (req, res) => {
@@ -20,6 +83,17 @@ router.get('/register', (req, res) => {
 router.get('/login', (req, res) => {
   res.render('login');
 });
+
+router.get('/logout', (req, res) => {
+	req.logout();
+	res.redirect('/');
+});
+
+router.post('/login',
+	passport.authenticate('local', { failureRedirect: '/login-error' }),
+  (req, res) => res.redirect('/'));
+
+router.get('/login-error', (req, res) => res.end("Login error"));
 
 router.post('/register', (req, res) => {
   if(req.body.password === req.body.password2){
